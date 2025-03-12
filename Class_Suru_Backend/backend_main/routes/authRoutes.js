611353a79f5fa47e71f07b78eda6b5108fe27c7f
youtanimstar <animdeep2019@gmail.com>
2025-03-12 10:20:49 +0530

    Refactor question creation and retrieval logic; update input validation and change POST to GET for fetching questions by exam ID

--- Class_Suru_Backend/backend_main/routes/authRoutes.js
+++ Class_Suru_Backend/backend_main/routes/authRoutes.js
@@ -8,31 +8,34 @@ import {
     verifyToken, 
     updateUser 
 } from "../controllers/authController.js";
-import { createExam, getExamById, updateExam } from "../controllers/examController.js";
+import { createExam, getExamById, updateExam, deleteExam, getExamBySubjectAndType } from "../controllers/examController.js";
 import { createQuestion, getQuestionsByExamId } from "../controllers/questionController.js";
 
 
 const router = express.Router();
 
+// User Apis
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);  // New route for password reset request
 router.post("/reset-password", resetPassword);    // New route to reset the password
 router.post("/user/:id", verifyToken, getUserDetails);
- 
-router.post("/exam", createExam); // done
-
-router.get("/exam/:examId", getExamById);
-
-// get subject name and type
-
 router.put("/user/update", verifyToken, updateUser);//  
 
+// Exam Apis
+router.post("/exam", createExam); // done
+router.get("/exam/:examId", getExamById);
+router.get("/exam/:subject/:type", getExamBySubjectAndType);
+router.put("/exam/update/:examId", updateExam);
+router.delete("/exam/delete/:examId", deleteExam);
+
+
+// Questions Apis
 router.post("/question/add", createQuestion);
-
 router.get("/question/exam/:exam_id", getQuestionsByExamId);
+router.get("/question/:question_id", getQuestionById);
 
-router.post("/exam/update/:examId", updateExam);
+
 
 // update question
 // get all questions by subject name, type, exam id
